129fd93834a0540bab57c113cef8ff706c3c8ccb
victordiazb00 <63013073+victordiazb00@users.noreply.github.com>
2021-06-10 11:28:56 +0200

    Partición de datos en train y test

--- practicaFinal.py
+++ practicaFinal.py
@@ -6,6 +6,8 @@ Nombre Estudiantes: Victor Diaz Bustos y Yunhao Lin Pan
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
 # Funcion para leer los datos del dataset de regresion
 def readData(archivo):
     datos = pd.read_csv(archivo, delim_whitespace=True, header=None)
@@ -18,7 +20,11 @@ def readData(archivo):
     return X, Y
 
 
+# Lectura de datos
 X, Y = readData('housing.data')
 
 print("X:", X)
 print("Y:", Y)
+
+# PARTICION: 70% train, 30% test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
