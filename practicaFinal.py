815bfc7c8f997c83d7a2e010e8b07cc6a6f72ca7
Yunhao <yunhi.0799@gmail.com>
2021-06-10 12:43:31 +0200

    ajuste de regresión lineal vanilla

--- practicaFinal.py
+++ practicaFinal.py
@@ -6,11 +6,13 @@ Nombre Estudiantes: Victor Diaz Bustos y Yunhao Lin Pan
 import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
+from scipy.sparse.construct import random
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
 
 pd.set_option('display.max_colwidth', None)
 # Funcion para leer los datos del dataset de regresion
@@ -77,7 +79,6 @@ def hyper_parameter_tuning_lineal_model(x_train, y_train):
         })
 
 
-    pd.set_option('display.max_colwidth', 0)
     df = DataFrame(scores, columns=["model", "best_score", "best_parameters"])
     return df
 
@@ -94,7 +95,7 @@ print(datos_df)
 # print("Y:", Y)
 
 # PARTICION: 70% train, 30% test
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=2)
 
 
 
@@ -112,7 +113,40 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 # y_train = data[:,-1]
 # print("Samples después de eliminar outliers: ", x_train.shape[0])
 
+
 # Estandarizacion de los datos usando el StandardScaler
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
+
+
+
+# Búsqueda de los mejores parámetros para regresión lineal
+df = hyper_parameter_tuning_lineal_model(x_train=X_train, y_train=Y_train)
+print(df)
+
+# %%
+# El modelo ridge funciona ligeramente mejor
+clf = linear_model.Ridge(max_iter=10000, alpha=2, fit_intercept=True, solver='lsqr', tol=0.001)
+clf.fit(X_train, Y_train)
+y_predicted = clf.predict(X_train)
+
+# Metrica de la bondad de los resultados dentro de la muestra
+print("Dentro de la muestra")
+print("MSE:", mean_squared_error(y_true=Y_train, y_pred=y_predicted))
+r2 = r2_score(y_true=Y_train, y_pred=y_predicted)
+adj_r2 = (1 - (1 - r2) * ((X_train.shape[0] - 1) / (X_train.shape[0] - X_train.shape[1] - 1)))
+print("R2:",r2)
+
+# R2 ajustado, para comparar entre modelos que no usen la misma cantidad de 
+#características
+print("R2 ajustado:",adj_r2)
+
+print("\nFuera de la muestra")
+# clf.fit(x_test, y_test)
+y_predicted = clf.predict(X_test)
+print("MSE:", mean_squared_error(y_true=Y_test, y_pred=y_predicted))
+r2 = r2_score(y_true=Y_test, y_pred=y_predicted)
+adj_r2 = (1 - (1 - r2) * ((X_test.shape[0] - 1) / (X_test.shape[0] - X_test.shape[1] - 1)))
+print("R2:",r2)
+print("R2 ajustado:",adj_r2)
